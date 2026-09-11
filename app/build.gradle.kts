cd93bbb1d7f2edd1cd98eb49f99fe2335947c86f
Карпов Олег Валентинович <KarpovOV@uralsib.ru>
2026-09-12 00:39:53 +0500

    1.0.10 update libraries dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
 }
 
 dependencies {
-//    implementation("com.github.karp4004:usbpdf:1.0.6")
-    implementation(project(":usbpdf"))
+    implementation("com.github.karp4004:usbpdf:1.0.10")
+//    implementation(project(":usbpdf"))
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
